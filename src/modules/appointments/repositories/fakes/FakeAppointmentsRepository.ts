85f69fb76d2a3f426b0de838fa02fee18434cffb
arthurramires <arthurramires1802@outlook.com>
2020-05-21 10:37:19 -0400

    Filtrando agendamentos por mês

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,7 +1,8 @@
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getYear } from 'date-fns';
 import IAppointmentsRepository from '@modules/appointments/repositories/iAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/iCreateAppointmentDTO';
+import IFindInAllMonthFromProviderDTO from '@modules/appointments/dtos/iFindInAllMonthFromProviderDTO';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
@@ -9,28 +10,41 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-      const findAppointment = this.appointments.find(appointment =>
-        isEqual(appointment.date, date),
-      );
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
+    );
 
-      return findAppointment;
+    return findAppointment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindInAllMonthFromProviderDTO): Promise<Appointment[]> {
+    const findAppointment = this.appointments.filter(
+      appointment =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year,
+    );
+
+    return findAppointment;
   }
 
   public async create({
     provider_id,
     date,
   }: ICreateAppointmentDTO): Promise<Appointment> {
-      const appointment = new Appointment();
+    const appointment = new Appointment();
 
+    appointment.id = uuid();
+    appointment.provider_id = provider_id;
+    appointment.date = date;
 
-      appointment.id = uuid();
-      appointment.provider_id = provider_id;
-      appointment.date = date;
+    this.appointments.push(appointment);
 
-      this.appointments.push(appointment);
-
-      return appointment;
-
+    return appointment;
   }
 }
 
