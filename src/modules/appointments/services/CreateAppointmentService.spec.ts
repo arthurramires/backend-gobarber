6c4ca34cf6bdcbd82bacb13d9719463a34a13db0
arthurramires <arthurramires1802@outlook.com>
2020-05-21 11:55:23 -0400

    Refatorando regras de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -12,8 +12,11 @@ describe('CreateAppointment', () => {
     );
   });
   it('should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
     const appoinment = await createAppointment.execute({
-      date: new Date(),
+      date: new Date(2020, 4, 10, 13),
       user_id: '1223',
       provider_id: '1212121212',
     });
@@ -29,12 +32,62 @@ describe('CreateAppointment', () => {
       provider_id: '1212121212',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         user_id: '1223',
         provider_id: '1212121212',
       }),
     ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appoinment on a past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 11),
+        user_id: 'user',
+        provider_id: '123123',
+      }),
+    );
+  });
+
+  it('should not be able to create an appoinment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 13),
+        user_id: '123123',
+        provider_id: '123123',
+      }),
+    );
+  });
+
+  it('should not be able to create an appoinment before 8am and after 5pm', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 7),
+        user_id: '123123',
+        provider_id: 'provider',
+      }),
+    );
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 18),
+        user_id: '123123',
+        provider_id: 'provider',
+      }),
+    );
   });
 });
