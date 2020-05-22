51ab375a9165e4793850df887dccf2b6b3f4441d
arthurramires <arthurramires1802@outlook.com>
2020-05-22 11:21:23 -0400

    Emails pelo Amazon SES

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,10 +1,11 @@
 import { container } from 'tsyringe';
-
+import mailConfig from '@config/mail';
 import IStorageProvider from './StorageProvider/models/iStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/iMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/iMailTemplateProvider';
 import HandlebarsMailProvider from './MailTemplateProvider/implementations/HadlebarMailProvider';
@@ -21,5 +22,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
