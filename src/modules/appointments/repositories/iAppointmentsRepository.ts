4e887b3c4363a8da66152a931ddc5ab09c27a90f
arthurramires <arthurramires1802@outlook.com>
2020-05-27 11:46:47 -0400

    Listando dias disponíveis do mês

--- src/modules/appointments/repositories/iAppointmentsRepository.ts
+++ src/modules/appointments/repositories/iAppointmentsRepository.ts
@@ -5,7 +5,7 @@ import IFindInAllDayFromProviderDTO from '../dtos/iFindInAllDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindInAllMonthFromProviderDTO,
   ): Promise<Appointment[]>;
